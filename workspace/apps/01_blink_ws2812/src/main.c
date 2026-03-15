755ade4b882e84d7e6416b5344bc498276d3d5f6
imochoa <18612100+imochoa@users.noreply.github.com>
2026-03-15 12:52:45 +0100

    added ws2812 example

--- workspace/apps/01_blink_ws2812/src/main.c
+++ workspace/apps/01_blink_ws2812/src/main.c
@@ -2,13 +2,13 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/led_strip.h>
-#include <zephyr/drivers/spi.h>
+#include <zephyr/driversa/spi.h>
 
 // Settings
 static const int32_t sleep_time_ms = 1000;
 
-#define STRIP_NODE		DT_ALIAS(led_strip)
-#define STRIP_NUM_PIXELS	DT_PROP(DT_ALIAS(led_strip), chain_length)
+#define STRIP_NODE DT_ALIAS(led_strip)
+#define STRIP_NUM_PIXELS DT_PROP(DT_ALIAS(led_strip), chain_length)
 
 static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
 static struct led_rgb pixels[STRIP_NUM_PIXELS];
@@ -32,7 +32,7 @@ int main(void)
 		state = !state;
 		printk("LED state: %d\r\n", state);
 
-		// Set the LED color
+		// Set the LED color (0-255 or 0x00-0xFF)
 		if (state)
 		{
 			pixels[0].r = 0x20;
