c738af2d3e045fd0f96daa455c8fb6fbe27c1d7a
imochoa <18612100+imochoa@users.noreply.github.com>
2026-03-16 08:14:27 +0100

    switched to LVGL

--- workspace/apps/10_demo_epaper/src/main.c
+++ workspace/apps/10_demo_epaper/src/main.c
@@ -1,82 +1,33 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/display.h>
-#include <string.h>
-
-#define DISPLAY_NODE DT_CHOSEN(zephyr_display)
+#include <lvgl.h>
 
 int main(void)
 {
-	const struct device *display = DEVICE_DT_GET(DISPLAY_NODE);
-	struct display_capabilities caps;
-	struct display_buffer_descriptor desc;
-	size_t buf_size;
-	uint8_t *buf;
+	const struct device *display = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 
 	printk("\n\n=== E-Paper Demo Start ===\n");
 
-	if (!device_is_ready(display)) {
+	if (!device_is_ready(display))
+	{
 		printk("Display device not ready\n");
 		return 0;
 	}
 
-	display_get_capabilities(display, &caps);
-	printk("Display: %ux%u, pixel format %u\n",
-	       caps.x_resolution, caps.y_resolution,
-	       caps.current_pixel_format);
+	lv_obj_set_style_bg_color(lv_scr_act(), lv_color_white(), 0);
+	lv_obj_set_style_bg_opa(lv_scr_act(), LV_OPA_COVER, 0);
 
-	buf_size = (caps.x_resolution * caps.y_resolution) / 8;
-	buf = k_malloc(buf_size);
-	if (!buf) {
-		printk("Failed to allocate framebuffer (%u bytes)\n",
-		       (unsigned int)buf_size);
-		return 0;
-	}
+	lv_obj_t *label = lv_label_create(lv_scr_act());
+	lv_obj_set_style_text_color(label, lv_color_black(), 0);
+	lv_label_set_text(label, "Hello, World!");
+	lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 
-	desc.width = caps.x_resolution;
-	desc.height = caps.y_resolution;
-	desc.pitch = caps.x_resolution;
-	desc.buf_size = buf_size;
+	lv_task_handler();
+	printk("Label displayed\n");
 
-	display_blanking_on(display);
-
-	memset(buf, 0xFF, buf_size);
-	display_write(display, 0, 0, &desc, buf);
-	display_blanking_off(display);
-	printk("Cleared to white\n");
-
-	k_sleep(K_SECONDS(3));
-
-	display_blanking_on(display);
-
-	memset(buf, 0x00, buf_size);
-	display_write(display, 0, 0, &desc, buf);
-	display_blanking_off(display);
-	printk("Filled black\n");
-
-	k_sleep(K_SECONDS(3));
-
-	display_blanking_on(display);
-
-	memset(buf, 0xFF, buf_size);
-
-	uint16_t stride = caps.x_resolution / 8;
-
-	for (uint16_t y = 0; y < caps.y_resolution; y++) {
-		for (uint16_t x_byte = 0; x_byte < stride; x_byte++) {
-			if ((y / 16 + x_byte / 4) % 2 == 0) {
-				buf[y * stride + x_byte] = 0x00;
-			}
-		}
-	}
-
-	display_write(display, 0, 0, &desc, buf);
-	display_blanking_off(display);
-	printk("Checkerboard pattern drawn\n");
-
-	k_free(buf);
-
-	while (1) {
+	while (1)
+	{
 		k_sleep(K_FOREVER);
 	}
 }
