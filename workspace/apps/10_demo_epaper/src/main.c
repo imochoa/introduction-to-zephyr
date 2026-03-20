879de289a82b8b567b68eb3189ec4b7a4ff723ae
imochoa <18612100+imochoa@users.noreply.github.com>
2026-03-20 21:03:43 +0100

    switched font

--- workspace/apps/10_demo_epaper/src/main.c
+++ workspace/apps/10_demo_epaper/src/main.c
@@ -14,13 +14,18 @@ int main(void)
 		printk("Display device not ready\n");
 		return 0;
 	}
+	lv_disp_t *disp = lv_disp_get_default();
+	// lv_disp_set_rotation(disp, LV_DISP_ROT_180); // Works!
+	// lv_disp_set_rotation(disp, LV_DISP_ROT_270);
+	lv_obj_t *screen = lv_scr_act();
 
-	lv_obj_set_style_bg_color(lv_scr_act(), lv_color_white(), 0);
-	lv_obj_set_style_bg_opa(lv_scr_act(), LV_OPA_COVER, 0);
+	lv_obj_set_style_bg_color(screen, lv_color_white(), 0);
+	lv_obj_set_style_bg_opa(screen, LV_OPA_COVER, 0);
 
-	lv_obj_t *label = lv_label_create(lv_scr_act());
+	lv_obj_t *label = lv_label_create(screen);
 	lv_obj_set_style_text_color(label, lv_color_black(), 0);
 	lv_label_set_text(label, "Hello, World!");
+	lv_obj_set_style_text_font(label, &lv_font_montserrat_20, 0);
 	lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 
 	lv_task_handler();
